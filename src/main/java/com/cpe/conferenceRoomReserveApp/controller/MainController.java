7d7aafcc0bcc9b8bac471c1a81ac159c740c5842
nontawatwuttikam <nontawatwuttikam@gmail.com>
2021-01-20 13:13:26 +0700

    Add On memory Security Authen and Add MainController

---
+++ src/main/java/com/cpe/conferenceRoomReserveApp/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.cpe.conferenceRoomReserveApp.controller;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/login.html")
+    public String login(Model model) {
+
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("username", loggedInUser.getName());
+        return "login.html";
+    }
+}
---
+++ src/main/java/com/cpe/conferenceRoomReserveApp/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.cpe.conferenceRoomReserveApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // [...]
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("Anon JansaiChon").password(passwordEncoder().encode("password"))
+                .roles("ADMIN").and().withUser("Nontawat Wuttikam").password(passwordEncoder().encode("password"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
