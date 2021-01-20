c5bb2df3b2682b62e2a3f51d0254ff2f7cbc96ab
nontawatwuttikam <nontawatwuttikam@gmail.com>
2021-01-20 10:11:20 +0700

    Add Spring Boot Project

---
+++ src/main/java/com/cpe/conferenceRoomReserveApp/ConferenceRoomReserveAppApplication.java
@@ -0,0 +1,13 @@
+package com.cpe.conferenceRoomReserveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomReserveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomReserveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpe/conferenceRoomReserveApp/ConferenceRoomReserveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.conferenceRoomReserveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomReserveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
